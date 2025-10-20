edbc147daefa73f0fffd933bbd3f296c28df621a
Yossi Cohen <yossi@example.com>
2025-10-20 12:59:38 +0300

    changes in the memberscontroller file

--- Controllers/MembersController.cs
+++ Controllers/MembersController.cs
@@ -8,19 +8,30 @@ namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class MembersController ( AppDbContext context) : ControllerBase
+    public class MembersController : ControllerBase
     {
+        private readonly AppDbContext context;
+
+        public MembersController(AppDbContext context)
+        {
+            this.context = context ?? throw new System.ArgumentNullException(nameof(context));
+        }
         [HttpGet] // GET: api/members
         public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
         {
-            var res = await context.Users.ToListAsync();
+            var res = context.Users == null ? new List<AppUser>() : await context.Users.ToListAsync();
             return Ok(res);
         }
 
         [HttpGet("{id}")] // GET: api/members/3
         public async Task<ActionResult<AppUser>> GetMember(string id)
         {
-            var res = await context.Users.FindAsync(id);
+            if (context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var res = await context.Users.FirstOrDefaultAsync(u => u.Id == id);
             if (res == null)
             {
                return NotFound();
@@ -31,6 +42,11 @@ namespace API.Controllers
         [HttpGet("by-name/{displayName}")] // GET: api/members/by-name/sagiv
         public ActionResult<AppUser> GetMemberByDisplayName(string displayName)
         {
+            if (context.Users == null)
+            {
+                return NotFound();
+            }
+
             var res = context.Users.FirstOrDefault(u => u.DisplayName == displayName);
             return res == null ? NotFound() : Ok(res);
         }
